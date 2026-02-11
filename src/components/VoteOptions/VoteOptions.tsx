85367189a0af9beec91355990d951aa19a6aef01
YuriDats <datsiuk.ukr@gmail.com>
2026-02-11 02:10:51 +0200

    fix types

--- src/components/VoteOptions/VoteOptions.tsx
+++ src/components/VoteOptions/VoteOptions.tsx
@@ -1,8 +1,8 @@
 import css from './VoteOptions.module.css';
-import type { Votes } from '../../types/votes';
+import type {VoteType} from '../../types/votes';
 
 interface VoteOptionsProps {
-    onVote: (type: keyof Votes) => void;
+    onVote: (type: VoteType) => void;
     onReset: () => void;
     canReset: boolean;
 }
